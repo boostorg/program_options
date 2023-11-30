1485e7aff0c05cf4e6a3a79b88de02e202742247
Markus Klemm <markus@markusklemm.net>
2023-11-30 22:52:42 +0100

    Fixed argc bug #127 in parser and added documentation

--- include/boost/program_options/parsers.hpp
+++ include/boost/program_options/parsers.hpp
@@ -120,7 +120,10 @@ namespace boost { namespace program_options {
         basic_command_line_parser(const std::vector<
                                   std::basic_string<charT> >& args);
         /** Creates a command line parser for the specified arguments
-            list. The parameters should be the same as passed to 'main'.
+            list. The parameters should be the same as passed to 'main', meaning:
+            @param argc Must be non-zero i.e. >= 0
+            @param argv argv[0] up to argv[argc-1] must point to null terminated strings and
+            argv[argc] must be 0 e.g. nullptr
         */
         basic_command_line_parser(int argc, const charT* const argv[]);
 
